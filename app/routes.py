fd3396b510a58499d63182a98ad1c4cac938fe0b
David M. Weigl <weigl@mdw.ac.at>
2022-07-13 17:57:41 +0200

    hackily include non-WPhil Donauwalzers

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,9 @@ from app import app
 @app.route("/")
 def index():
     return render_template('index.html')
+@app.route("/other")
+def other():
+    return render_template('other.html')
 @app.route("/old")
 def old():
     return render_template('old.html')

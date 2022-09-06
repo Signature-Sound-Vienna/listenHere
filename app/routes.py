59a0b3008700f54e1d8bb20011e450f946091278
David M. Weigl <weigl@mdw.ac.at>
2022-09-06 13:56:13 +0200

    add Rosegarden experimental alignments

--- app/routes.py
+++ app/routes.py
@@ -28,4 +28,6 @@ def midi_test():
 @app.route("/rosegarden")
 def rosegarden():
     return render_template('listen.html', 
-            data=url_for('static', filename='align/Rosegarden-midi.json'))
+            #data=url_for('static', filename='align/Rosegarden-midi.json'))
+            #data=url_for('static', filename='align/allDonauRosegarden.json'))
+            data=url_for('static', filename='align/allFledermausRosegarden.json'))

d650cc7cf5431c148092fe45e07a84a065ce1c42
David M. Weigl <weigl@mdw.ac.at>
2025-07-02 16:32:46 +0200

    prettier formatting

--- app/static/js/linked-data.js
+++ app/static/js/linked-data.js
@@ -1,18 +1,18 @@
 // namespace definitions
 export const nsp = {
-  DCT: 'http://purl.org/dc/terms/',
-  FOAF: 'http://xmlns.com/foaf/0.1/',
-  FRBR: 'http://purl.org/vocab/frbr/core#',
-  LDP: 'http://www.w3.org/ns/ldp#',
-  MAO: 'https://domestic-beethoven.eu/ontology/1.0/music-annotation-ontology.ttl#',
-  OA: 'http://www.w3.org/ns/oa#',
-  PIM: 'http://www.w3.org/ns/pim/space#',
-  RDF: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-  RDFS: 'http://www.w3.org/2000/01/rdf-schema#',
-  SCHEMA: 'https://schema.org/'
+  DCT: "http://purl.org/dc/terms/",
+  FOAF: "http://xmlns.com/foaf/0.1/",
+  FRBR: "http://purl.org/vocab/frbr/core#",
+  LDP: "http://www.w3.org/ns/ldp#",
+  MAO: "https://domestic-beethoven.eu/ontology/1.0/music-annotation-ontology.ttl#",
+  OA: "http://www.w3.org/ns/oa#",
+  PIM: "http://www.w3.org/ns/pim/space#",
+  RDF: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+  RDFS: "http://www.w3.org/2000/01/rdf-schema#",
+  SCHEMA: "https://schema.org/",
 };
 
-export const politeness=200; // milliseconds between network requests
+export const politeness = 200; // milliseconds between network requests
 
 /**
  * Recursively traverse a graph of linked data (JSON-LD documents) starting at url
@@ -34,52 +34,72 @@ export const politeness=200; // milliseconds between network requests
 export async function traverseAndFetch(
   url,
   targetTypes,
-  { typeToHandlerMap = {}, followList = [], blockList = [], userProvided = true, jumps = 10, fetchMethod = fetch } = {}
+  {
+    typeToHandlerMap = {},
+    followList = [],
+    blockList = [],
+    userProvided = true,
+    jumps = 10,
+    fetchMethod = fetch,
+  } = {}
 ) {
   fetchMethod(url, {
     headers: {
-      Accept: 'application/ld+json',
+      Accept: "application/ld+json",
     },
   })
     .then((resp) => {
       if (resp.status >= 400) {
-        console.warn("Couldn't traverseAndFetch resource: ", url, resp.status)
+        console.warn("Couldn't traverseAndFetch resource: ", url, resp.status);
       } else {
         return resp.json();
       }
     })
     .then((json) => jsonld.expand(json))
     .then((expanded) => {
-      console.log('expanded: ', expanded);
+      console.log("expanded: ", expanded);
       let resourceDescription;
       if (Array.isArray(expanded)) {
         // got an array back - find the node matching the current document (either absolute or relative URI)
         // (n.b. if there are multiple matches this will just return the first encountered - TODO consider alternatives)
-        resourceDescription = expanded.find((o) => o['@id'] === url.href || o['@id'] === '' || o['@id'] === './');
-        if (!resourceDescription && !url.href.endsWith('/')) {
+        resourceDescription = expanded.find(
+          (o) => o["@id"] === url.href || o["@id"] === "" || o["@id"] === "./"
+        );
+        if (!resourceDescription && !url.href.endsWith("/")) {
           // try again with trailing slash
-          resourceDescription = expanded.find((o) => o['@id'] === url.href + '/');
+          resourceDescription = expanded.find(
+            (o) => o["@id"] === url.href + "/"
+          );
         }
       } else {
         resourceDescription = expanded;
       }
-      console.log('resource description: ', resourceDescription);
-      if (resourceDescription && '@type' in resourceDescription) {
-        if (!Array.isArray(resourceDescription['@type'])) {
+      console.log("resource description: ", resourceDescription);
+      if (resourceDescription && "@type" in resourceDescription) {
+        if (!Array.isArray(resourceDescription["@type"])) {
           // ensure array
-          resourceDescription['@type'] = [resourceDescription['@type']];
+          resourceDescription["@type"] = [resourceDescription["@type"]];
         }
         console.log("TARGET TYPES: ", targetTypes);
         const targetUrlStrings = targetTypes.map((t) => t.href);
-        if (resourceDescription['@type'].filter((t) => targetUrlStrings.includes(t)).length) {
+        console.log("TARGET URL STRINGS: ", targetUrlStrings);
+        console.log(
+          "resourceDescription['@type']: ",
+          resourceDescription["@type"]
+        );
+        if (
+          resourceDescription["@type"].filter((t) =>
+            targetUrlStrings.includes(t)
+          ).length
+        ) {
           // found a target resource["@id"]!
           ingestExternalResource(url, typeToHandlerMap, resourceDescription);
         }
         if (jumps >= 0) {
           // attempt to continue traversal
           const followUrlStrings = followList.map((l) => l.href);
-          let connectionsToFollow = Object.keys(resourceDescription).filter((predicate) =>
-            followUrlStrings.includes(predicate)
+          let connectionsToFollow = Object.keys(resourceDescription).filter(
+            (predicate) => followUrlStrings.includes(predicate)
           );
           connectionsToFollow.forEach(async (pred) => {
             // ensure array
@@ -91,10 +111,12 @@ export async function traverseAndFetch(
             predObjects.forEach((obj) => {
               try {
                 // recur if object is a URL and not in block list
-                if (!'@id' in obj || blockUrlStrings.includes(obj['@id'])) {
-                  console.warn("Couldn't raverse and fetch: target is a literal or target URI on blockList");
+                if (!"@id" in obj || blockUrlStrings.includes(obj["@id"])) {
+                  console.warn(
+                    "Couldn't traverse and fetch: target is a literal or target URI on blockList"
+                  );
                 }
-                let objUrl = new URL(obj['@id']);
+                let objUrl = new URL(obj["@id"]);
                 // politely continue traversal
                 setTimeout(
                   () =>
@@ -104,7 +126,7 @@ export async function traverseAndFetch(
                       blockList: [new URL(url.href), ...blockList],
                       userProvided: false,
                       jumps: jumps - 1,
-                      fetchMethod
+                      fetchMethod,
                     }),
                   politeness
                 );
@@ -115,7 +137,11 @@ export async function traverseAndFetch(
           });
         }
       } else {
-        console.warn('Found no matching resource description in ', url.href, resourceDescription);
+        console.warn(
+          "Found no matching resource description in ",
+          url.href,
+          resourceDescription
+        );
       }
     });
 } // traverseAndFetch()
@@ -129,16 +155,25 @@ export async function traverseAndFetch(
 export function ingestExternalResource(url, typeToHandlerMap, resource) {
   try {
     // ensure array
-    resource['@type'] = Array.isArray(resource['@type']) ? resource['@type'] : [resource['@type']];
-    const mappedTypes = Object.keys(typeToHandlerMap).filter((t) => resource['@type'].includes(t));
+    resource["@type"] = Array.isArray(resource["@type"])
+      ? resource["@type"]
+      : [resource["@type"]];
+    const mappedTypes = Object.keys(typeToHandlerMap).filter((t) =>
+      resource["@type"].includes(t)
+    );
     // call each relevant (type-matching) callback on the resource
-    console.log("ingest external resource: ", mappedTypes, typeToHandlerMap, resource)
+    console.log(
+      "ingest external resource: ",
+      mappedTypes,
+      typeToHandlerMap,
+      resource
+    );
     mappedTypes.forEach((t) => {
-      'args' in typeToHandlerMap[t] ? 
-        typeToHandlerMap[t].func(resource, url, ...typeToHandlerMap[t].args) : 
-        typeToHandlerMap[t].func(resource, url)  
+      "args" in typeToHandlerMap[t]
+        ? typeToHandlerMap[t].func(resource, url, ...typeToHandlerMap[t].args)
+        : typeToHandlerMap[t].func(resource, url);
     });
   } catch (e) {
     console.error("Couldn't ingest external resource: ", e);
   }
-}// ingestExternalResource()
+} // ingestExternalResource()

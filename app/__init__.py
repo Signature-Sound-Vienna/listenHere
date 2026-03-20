82942a201247528bda0d0ccd3200fc467c6d882b
David M. Weigl <weigl@mdw.ac.at>
2026-03-21 00:10:25 +0100

    bump version

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['VERSION'] = '0.14.0'
+app.config['VERSION'] = '0.15.0'
 app.config['VERSION_DATE'] = '20 March 2026'
 
 @app.context_processor

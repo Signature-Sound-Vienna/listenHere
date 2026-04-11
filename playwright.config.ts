35beec64bdd34ebacff120bb9931f5470cf70cb0
David M. Weigl <weigl@mdw.ac.at>
2026-04-11 21:20:48 +0200

    regex fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
     },
     {
       name: 'functional-firefox',
-      testMatch: /(?!.*\.(perf|solid)\.spec\.).*\.spec\.ts/,
+      testMatch: /^(?!.*\.(perf|solid)\.spec\.).*\.spec\.ts$/,
       use: { ...devices['Desktop Firefox'] },
     },
     {

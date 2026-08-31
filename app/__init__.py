acd7104dc12e35e03fe01ee67d58a56e1de9c7ed
David M. Weigl <weigl@mdw.ac.at>
2026-08-31 22:22:33 +0200

    Correct date

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 app.config['VERSION'] = '0.44.0'
-app.config['VERSION_DATE'] = '1 September 2026'
+app.config['VERSION_DATE'] = '31 August 2026'
 app.config['VERSION_DATE_ISO'] = datetime.strptime(app.config['VERSION_DATE'], '%d %B %Y').strftime('%Y-%m-%d')
 
 @app.context_processor
